7ab00728dd79e029aee62199ab19fb8fb6210cd1
Containerxox <containerxox@gmail.com>
2026-05-28 10:42:48 +0900

    fix: pom.xml과 RedisSessionConfig.java 수정

--- src/main/java/com/example/petcarelog/config/RedisSessionConfig.java
+++ src/main/java/com/example/petcarelog/config/RedisSessionConfig.java
@@ -3,8 +3,10 @@ package com.example.petcarelog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
+@EnableRedisHttpSession
 public class RedisSessionConfig {
 
     @Bean
